387804efce29ffbb1bfb02e9e663b453e5be6ebf
Shristi Sharma <ssharma@unc.edu>
2021-08-26 10:27:38 -0400

    New Files

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,4 @@
-"""1st program in COMP110"""
+"""My first program for COMP110."""
 
 __author__ = "730478650"
-
 print("Hello, world.")
---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates aking the user for input"""
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name)
