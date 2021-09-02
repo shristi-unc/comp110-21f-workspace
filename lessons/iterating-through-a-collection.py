866528dd0973133d3ab47f0d0bd7b9a6d40e8315
Shristi Sharma <ssharma@unc.edu>
2021-09-02 10:40:00 -0400

    LS08 and LS09

---
+++ lessons/iterating-through-a-collection.py
@@ -0,0 +1,9 @@
+"""Example of looping through all characters in a string."""
+
+user_string: str = input("Give me a string!")
+
+i: int = 0
+
+while i < len(user_string):
+    print(user_string[i])
+    i = i + 1
